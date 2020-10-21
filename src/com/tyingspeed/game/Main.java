fe2618ce38993518a55bf3687c9d8ee39f87d9de
BingNa Yang <bingnayang@gmail.com>
2020-10-20 22:50:48 -0400

    setup method to calculate word per minute

--- src/com/tyingspeed/game/Main.java
+++ src/com/tyingspeed/game/Main.java
@@ -1,11 +1,22 @@
 package com.tyingspeed.game;
 
+import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		TypingSpeedGame game = new TypingSpeedGame();
-		game.getTestingSentence();
+		System.out.println(game.getTestingSentence());
+		
+		double startTime = LocalTime.now().toNanoOfDay();
+		
+		Scanner scanner = new Scanner(System.in);
+		String typedSentence = scanner.nextLine();
+		
+		double endTime = LocalTime.now().toNanoOfDay();
+		game.getWordPerMinute(startTime,endTime);
+				
 	}
 
 }
--- src/com/tyingspeed/game/TypingSpeedGame.java
+++ src/com/tyingspeed/game/TypingSpeedGame.java
@@ -3,18 +3,29 @@ package com.tyingspeed.game;
 import java.util.Random;
 
 public class TypingSpeedGame {
-	private String[] testingSentences = {"In order to have a complete sentence, the sentence must have a minimum of three word types: a subject, a verb, and an object. In most cases, the subject is a noun or a pronoun.",
+	private String[] testingSentences = {"In order to have a complete sentence, the sentence must have a minimum of three word types: a subject, a verb, and an object.",
 								"There are always situations when writers have difficulties getting their words down on paper.",
 								"We hope that you have found this sentence tool to be fun and interesting."};
-	private int wordPerMin;
+	private int wordPerMinute;
 	private double accuracyPercent;
 	
-	public void getTestingSentence() {
+	public String getTestingSentence() {
 		Random random = new Random();
 		int randSentence = random.nextInt((testingSentences.length));
 		System.out.println(randSentence);
+		return testingSentences[randSentence];
 	}
 	
+	public void getWordPerMinute(double startTime, double endTime) {
+		double elapsedTime = endTime - startTime;
+		double seconds = elapsedTime / 1000000000.0;
+
+		System.out.println("ElapsedTime Start: "+startTime);
+		System.out.println("ElapsedTime End: "+endTime);
+		System.out.println("ElapsedTime: "+elapsedTime);
+		System.out.println("Second: "+seconds);
+
+	}
 	
 
 }
